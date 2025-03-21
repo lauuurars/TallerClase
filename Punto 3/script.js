7adcb8263a200f046a55d350488685d8c256fa28
lauuurars <1120098177@u.icesi.edu.co>
2025-03-21 16:54:28 -0500

    se realizó punto 3

---
+++ Punto 3/script.js
@@ -0,0 +1,18 @@
+
+// c significa calificación
+let c1= 5;
+let c2= 2;
+let c3= 3;
+let c4= 1;
+let c5= 1;
+
+let promedio = (c1+c2+c3+c4+c5)/5
+
+console.log(promedio); 
+
+if (promedio >= 3){
+    console.log("El estudiante aprobó");   
+} else{
+    console.log("El estudiante no aprobó");
+}
+
